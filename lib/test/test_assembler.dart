9b0d3ee60edd0cace7c18ded9169c7a0c92a24af
josh <joshuajarvis0711@gmail.com>
2021-09-30 15:52:27 -0600

    Added unit tests for add function.

--- lib/test/test_assembler.dart
+++ lib/test/test_assembler.dart
@@ -8,4 +8,23 @@ void testAdd() {
     obj.writeAdd();
     expect(4163, obj.bCommands[0]);
   });
+
+  test('Successfully add with immediate value.', () {
+    var obj = Lc3DartAssembler();
+    obj.commands = ['ADD', 'R7', 'R3', '30'];
+    obj.writeAdd();
+    expect(7934, obj.bCommands[0]);
+  });
+
+  test('Fail to add with mis-named register.', () {
+    var obj = Lc3DartAssembler();
+    obj.commands = ['ADD', 'R0', 'R1', 'R8'];
+    expect(obj.writeAdd, throwsException);
+  });
+
+  test('Fail to add with overflow immediate value.', () {
+    var obj = Lc3DartAssembler();
+    obj.commands = ['ADD', 'R7', 'R3', '32'];
+    expect(obj.writeAdd, throwsException);
+  });
 }
